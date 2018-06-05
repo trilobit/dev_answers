ec96ba59e7cd89c60438aec9359d8dafc7389a0f
Alexander Savchuk <trilobit.alex@gmail.com>
2018-06-05 18:26:10 +0300

    added first developer answers into database

---
+++ index.js
@@ -0,0 +1,10 @@
+const database = [
+  {
+    'text': 'То что мне понравилось, я лайкнул!',
+    'copyright': 'https://github.com/enemis',
+  },
+  {
+    'text': 'Это какая-то смесь функционального программирования и говнокода!',
+    'copyright': 'https://github.com/enemis',
+  }
+];
