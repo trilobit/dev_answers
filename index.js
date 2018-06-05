2721d405a5fc8bc35989bee52d6012f27c2ff145
trilobit <trilobit.alex@gmail.com>
2018-06-05 19:20:10 +0300

    simple page

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const database = [
   {
-    'text': 'То что мне понравилось, я лайкнул!',
-    'copyright': 'https://github.com/enemis',
+      'text': 'То что мне понравилось, я лайкнул!',
+      'url': 'https://github.com/enemis',
+      'name': 'Andrey S.',
   },
   {
-    'text': 'Это какая-то смесь функционального программирования и говнокода!',
-    'copyright': 'https://github.com/enemis',
+      'text': 'Это какая-то смесь функционального программирования и говнокода!',
+      'url': 'https://github.com/enemis',
+      'name': 'Andrey S.',
   }
 ];
